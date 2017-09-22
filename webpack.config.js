fd1ff92681fe842bc6a9e0fe65d845692d1a5695
Rachael Njeri <rachael.njeri@andela.com>
2017-09-22 09:46:11 +0300

    configure webpack
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.(css)$|scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+};
